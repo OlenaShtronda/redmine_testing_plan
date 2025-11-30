b1ea84b62e517b3cd3ffc0277b6fab1a9abc4819
Olena Shtronda <e.shtronda@gmail.com>
2025-11-30 19:06:27 +0200

    add allure report

--- playwright.config.js
+++ playwright.config.js
@@ -14,16 +14,26 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // Updated to use Allure + HTML reporter
+  reporter: [
+    ['html'],
+    ['allure-playwright', { outputFolder: 'allure-results' }]
+  ],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -82,4 +92,3 @@ export default defineConfig({
     timeout: 10000,
   },
 });
-
